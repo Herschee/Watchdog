3cd8489159613df8d361b4622cd0263f5414f7b6
Henry Wrightman <wrigh928@umn.edu>
2015-09-19 00:57:39 -0500

    Working timing & buttons.
    Weinersalad

---
+++ src/clock.c
@@ -0,0 +1,86 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+
+static TextLayer *s_uptime_layer;
+
+static int s_uptime = 0;
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  // Use a long-lived buffer
+  static char s_uptime_buffer[32];
+
+  // Get time since launch
+  int seconds = s_uptime % 60;
+  int minutes = (s_uptime % 3600) / 60;
+  int hours = s_uptime / 3600;
+
+  // Update the TextLayer
+  snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "Uptime: %dh %dm %ds", hours, minutes, seconds);
+  text_layer_set_text(s_uptime_layer, s_uptime_buffer);
+
+  // Increment s_uptime
+  s_uptime++;
+
+  // handle flagging
+  if (seconds >= 24) {
+     // warning extension
+  }
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Up");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Down");
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  // Create output TextLayer
+  s_uptime_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
+  text_layer_set_text_alignment(s_uptime_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_uptime_layer, "Uptime: 0h 0m 0s");
+  layer_add_child(window_layer, text_layer_get_layer(s_uptime_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  // Subscribe to TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+	.load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
