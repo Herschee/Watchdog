59f24d451eff552396f5e0c20b6ee99c7650485c
Henry Wrightman <wrigh928@umn.edu>
2015-09-19 14:58:49 -0500

    Added settings js (app.js); configurable via phone
    Held Nikita's hand

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-Pebble.addEventListener('showConfiguration', function(e) {
-  // Show config page
-  Pebble.openURL('https://reddit.com');
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
 });
 
-// Close config page and return data
-location.href = 'pebblejs://close#success';
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'https://cdn.rawgit.com/Herschee/Watchdog/master/settingsPage/index.html';
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+	// configure this dictionary you fucking idiot to save the settings from the rawgit
+	// don't know what a dictionary is? https://en.wikipedia.org/wiki/Associative_array
+  var dict = {};
+	// for EXAMPLE..
+  dict['KEY_PHONE_NUMBER'] = configData['phone_number'];
+
+	// Send this shit to the app, obviously
+  Pebble.sendAppMessage(dict, function() {
+    console.log('Send successful: ' + JSON.stringify(dict));
+  }, function() {
+    console.log('Send failed!');
+  });
+});
