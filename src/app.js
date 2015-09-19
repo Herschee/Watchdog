854d362886e4430b598d1e79c51b4beb276c073d
Nikita Yurkov <yurko006@umn.edu>
2015-09-19 15:16:06 -0500

    Made developer url

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://cdn.rawgit.com/Herschee/Watchdog/master/settingsPage/index.html';
+  var url = 'https://rawgit.com/Herschee/Watchdog/master/settingsPage/index.html';
   console.log('Showing configuration page: ' + url);
 
   Pebble.openURL(url);
