8b0bf0371790c0e8210bc38e3ce333d889f73326
Nikita Yurkov <yurko006@umn.edu>
2015-09-19 14:41:20 -0500

    Henry suggested this

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@ Pebble.addEventListener('showConfiguration', function(e) {
   // Show config page
   Pebble.openURL('https://reddit.com');
 });
+
+// Close config page and return data
+location.href = 'pebblejs://close#success';
